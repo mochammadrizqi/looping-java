e7c9de7a2500083be64c6f780051407fd7c7eb08
SILVER <SILVER@LAPTOP-M6VEPCH3>
2018-08-02 07:47:14 +0700

    For While Do-While

---
+++ src/j08/Contoh1.java
@@ -0,0 +1,31 @@
+
+package j08;
+
+
+public class Contoh1 {
+    public static void main(String[] args) {
+        //PENGULANGAN FOR
+        int x;
+        for (x = 1; x <= 5; x++)
+        {
+           System.out.println("SMK TELKOM Malang"); 
+        }
+        
+        //PENGULANGAN WHILE
+        int i = 1;
+        while(1<=5)
+        {
+            System.out.println("SMK TELKOM Malang");
+            i++;
+        }
+        
+        //PENGULANGAN DO WHILe
+        int j = 1;
+        do
+        {
+            System.out.println("SMK TELKOM Malang");
+            j++;
+        }
+        while (j <=5);
+    }
+}
